6211381cb867ec76b4fec37366624a5d519bf644
Andy Tran <aktran321@gmail.com>
2020-01-03 01:15:44 -0500

    adding previous changes

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,39 +1,47 @@
 import React, { Component } from "react";
-import ImageCard from "./components/ImageCard";
+import Card from "./components/ImageCard";
 import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
+import Header from "./components/Header";
 import images from "./images.json";
+import Footer from "./components/Footer";
 
 class App extends Component {
     //setting this.state.images to the images.json array
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-        images:images,
-        topScore:0,
-        currentScore:0
-    };
-    this.checkIfClicked = this.checkIfClicked.bind(this);
-}
-    checkIfClicked = id => {
+            images:images,
+            topScore:0,
+            currentScore:0
+        };
+        this.checkIfClicked = this.checkIfClicked.bind(this);
+    }
+    checkIfClicked = function (id)  {
+        //variables
         let clickedCard = this.state.images.filter(image => image.id === id)[0];
         let imagesCopy = this.state.images.slice().sort(function(a, b){return 0.5 - Math.random()});
+        //if card hasn't been clicked, set to true
         if(!clickedCard.clicked) {
             clickedCard.clicked = true;
+            //shuffle the cards
             imagesCopy[imagesCopy.findIndex((card) => card.id === id)] = clickedCard;
             this.setState({
+                //set the new array
                 images: imagesCopy,
+                //change scores
                 currentScore: this.state.currentScore + 1,
                 topScore: (this.state.currentScore + 1 > this.state.topScore ? this.state.currentScore + 1 : this.state.topScore)
             });
         } else {
-            let resetImagesCopy = imagesCopy.map((image) => {
+            //reset the game 
+            let resetImagesCopy = imagesCopy.map((card) => {
                 return {
-                    id:image.id,
-                    image:image.image,
+                    id:card.id,
+                    image:card.image,
                     clicked: false,
                 }
             });
+            //reset the score
             this.setState({
                 images:resetImagesCopy,
                 currentScore: 0,
@@ -42,21 +50,22 @@ class App extends Component {
     }
     render() {
         return (
+            <div className = "container">
+                <Header currentScore = {this.state.currentScore} topScore={this.state.topScore}/>
             <Wrapper>
-                <Title>Image Memory Game</Title>
                     {this.state.images.map(picture => (
-                            <ImageCard
+                            <Card
                             checkifClicked={this.checkIfClicked}
                             id={picture.id}
                             key={picture.id}
-                            name={picture.name}
                             image={picture.image}
                             
                             />
                 ))}
             </Wrapper>
+            <Footer/>
+            </div>
         )
     }
-   
 }
 export default App;
