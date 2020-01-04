ec50dfa9b7c19b867760c73dcdb006534996aaa8
Andy Tran <aktran321@gmail.com>
2020-01-04 03:21:25 -0500

    After a few weeks, came back to this project and found a typo in line 60 of App.js that was crashing the game...

--- memory/src/App.js
+++ memory/src/App.js
@@ -1,71 +1,73 @@
 import React, { Component } from "react";
-import Card from "./components/ImageCard";
+import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import images from "./images.json";
 import Footer from "./components/Footer";
 
-class App extends Component {
-    //setting this.state.images to the images.json array
+class App extends Component {  
+     //setting this.state.images to the images.json array
     constructor(props) {
-        super(props);
-        this.state = {
-            images:images,
-            topScore:0,
-            currentScore:0
-        };
-        this.checkIfClicked = this.checkIfClicked.bind(this);
+    super(props);
+    this.state = {
+        images:images,
+        topScore:0,
+        currentScore:0
+    };
+    this.checkIfClicked = this.checkIfClicked.bind(this);
+}
+checkIfClicked(id)  {
+    //variables
+    let clickedCard = this.state.images.filter(card => card.id === id)[0];
+    let imagesCopy = this.state.images.slice().sort(function(a, b){return 0.5 - Math.random()});
+    //if card hasn't been clicked, set to true
+    if(!clickedCard.clicked) {
+        clickedCard.clicked = true;
+        //shuffle the images
+        imagesCopy[imagesCopy.findIndex((card) => card.id === id)] = clickedCard;
+        this.setState({
+            //set the new array
+            images: imagesCopy,
+            //change scores
+            currentScore: this.state.currentScore + 1,
+            topScore: (this.state.currentScore + 1 > this.state.topScore ? this.state.currentScore + 1 : this.state.topScore)
+        });
+    } else {
+           //reset the game 
+        let resetimagesCopy = imagesCopy.map((card) => {
+            return {
+                id:card.id,
+                image:card.image,
+                clicked: false,
+            }
+        });
+        //reset the score
+        this.setState({
+            images:resetimagesCopy,
+            currentScore: 0,
+        });
     }
-    checkIfClicked = function (id)  {
-        //variables
-        let clickedCard = this.state.images.filter(image => image.id === id)[0];
-        let imagesCopy = this.state.images.slice().sort(function(a, b){return 0.5 - Math.random()});
-        //if card hasn't been clicked, set to true
-        if(!clickedCard.clicked) {
-            clickedCard.clicked = true;
-            //shuffle the cards
-            imagesCopy[imagesCopy.findIndex((card) => card.id === id)] = clickedCard;
-            this.setState({
-                //set the new array
-                images: imagesCopy,
-                //change scores
-                currentScore: this.state.currentScore + 1,
-                topScore: (this.state.currentScore + 1 > this.state.topScore ? this.state.currentScore + 1 : this.state.topScore)
-            });
-        } else {
-            //reset the game 
-            let resetImagesCopy = imagesCopy.map((card) => {
-                return {
-                    id:card.id,
-                    image:card.image,
-                    clicked: false,
-                }
-            });
-            //reset the score
-            this.setState({
-                images:resetImagesCopy,
-                currentScore: 0,
-            });
-        }
-    }
-    render() {
+}
+render() {
         return (
             <div className = "container">
                 <Header currentScore = {this.state.currentScore} topScore={this.state.topScore}/>
             <Wrapper>
-                    {this.state.images.map(picture => (
+                    {this.state.images.map(card => (
                             <Card
-                            checkifClicked={this.checkIfClicked}
-                            id={picture.id}
-                            key={picture.id}
-                            image={picture.image}
-                            
+                            //FINALLY FOUND OUT THERE WAS A TYPO IN THE LINE BELOW
+                            //checkifClicked --> checkIfClicked WOWWWWWW
+                            checkIfClicked={this.checkIfClicked}
+                            id={card.id}
+                            key={card.id}
+                            image={card.image}
                             />
                 ))}
             </Wrapper>
-            <Footer/>
+            <Footer />
             </div>
-        )
+        );
     }
 }
 export default App;
+
