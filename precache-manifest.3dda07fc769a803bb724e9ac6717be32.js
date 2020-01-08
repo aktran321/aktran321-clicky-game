3b483bd38190b70453dbb5a4e3e05867fb268ac5
Andy Tran <aktran321@gmail.com>
2020-01-08 04:54:30 -0500

    Updates

---
+++ precache-manifest.3dda07fc769a803bb724e9ac6717be32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646fedefba6dd212a8e4638ba4154e0a",
+    "url": "/aktran321-clicky-game/index.html"
+  },
+  {
+    "revision": "6679fd0c7e6cb89bafeb",
+    "url": "/aktran321-clicky-game/static/css/main.51a8e340.chunk.css"
+  },
+  {
+    "revision": "e46c37ee7c9ca361ab9d",
+    "url": "/aktran321-clicky-game/static/js/2.4610239f.chunk.js"
+  },
+  {
+    "revision": "6679fd0c7e6cb89bafeb",
+    "url": "/aktran321-clicky-game/static/js/main.a40f25f2.chunk.js"
+  },
+  {
+    "revision": "ea8cb3c759894f71c9dd",
+    "url": "/aktran321-clicky-game/static/js/runtime-main.0067b653.js"
+  }
+]);
